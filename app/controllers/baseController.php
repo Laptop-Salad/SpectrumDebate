feb34efd22cc6ff9e227451498cae43904b4332f
Laptop-Salad <email@example.com>
2023-07-19 18:22:59 +0100

    refactor base controller

--- app/controllers/baseController.php
+++ app/controllers/baseController.php
@@ -4,13 +4,16 @@ class BaseController
     private $forwarding;
     function baseConstruct()
     {
+        /**
+         * All controllers call this function first
+         */
         session_start();
     }
-        
+
     function ensureUserLoggedIn($redirect = "")
     {
         /**
-         * if user isn't logged in, sends user to home
+         * If user isn't logged in, sends user to home
          */
         if (!isset($_SESSION["username"])) {
             echo $this->getRedirect("login");
@@ -19,6 +22,12 @@ class BaseController
     }
 
     function getRedirect($location = "", $forwarding = "") {
+        /**
+         * Gets a javascript redirect script
+         * 
+         * @param string $location where to redirect, e.g. dashboard, login
+         * @param string $forwarding A script showing a popup to display, e.g. successful acc. creation
+         */
         if ($forwarding == "") {
             return "<script type='text/javascript'>window.location.href='//localhost/$location'</script>";
         } else {
@@ -26,10 +35,19 @@ class BaseController
         }
     }
 
-    function displayContent($filename, $pageTitle, $variables, $stylesheet = null) {
+    function displayContent($filename, $pageTitle, $variables) {
+        /**
+         * Displays all required content
+         * 
+         * @param string $filename the pug/html file to display
+         * @param string $pageTitle the title of the page, e.g. Home
+         * @param array $variables variables to pass to the pug file
+         */
+
         $variables["domain"] = "//localhost";
         $variables["title"] = $pageTitle;
 
+        // Username to display in navbar
         if (isset($_SESSION["username"])) {
             $variables["username"] = $_SESSION["username"];
         } else {
