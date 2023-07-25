2900496c73fc92e3836f551373c677065d93ad68
Laptop-Salad <email@example.com>
2023-07-25 10:30:06 +0100

    add confirm delete

--- app/scripts/dashboard.js
+++ app/scripts/dashboard.js
@@ -10,3 +10,21 @@ nsBtn.addEventListener('click', () => {
 nsCloseBtn.addEventListener('click', () => {
     nsModal.close();
 })
+
+// Confirm delete account
+const delBtn = document.getElementById("delAcc");
+
+delBtn.addEventListener('click', () => {
+    console.log("hhhh");
+    Swal.fire({
+        title: 'Delete account? You can\'t undo this action!',
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+        denyButtonText: 'No',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = delBtn.getAttribute("href");
+        }
+      })
+      
+})
