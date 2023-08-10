5d64238fe5e267a2d958301dd27442e7501b809f
Laptop-Salad <email@example.com>
2023-08-10 16:54:50 +0100

    add bootstrap and change dashboard, navbar

--- app/scripts/base.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("menu").addEventListener('click', () => {
-    let header = document.getElementById("smallerMenu");
-    header.style.display = "flex";
-});
-
-document.getElementById("closeMenu").addEventListener('click', () => {
-    let header = document.getElementById("smallerMenu");
-    header.style.display = "none";
-});
